da9db5796e2669cb90eb3f7376c6951b9d2f87cd
Nattarika-Cha <s5902041620113@email.kmutnb.ac.th>
2020-12-16 20:41:19 +0700

    product

--- src/content/Product.js
+++ src/content/Product.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
-import { Container } from 'react-bootstrap';
+import { Row, Col } from 'antd';
+import { Container, } from 'react-bootstrap';
+import '../css/Product.css';
+
+import ProductSlide from "./Product/ProductSlide"
 
 export default class Product extends Component {
     constructor(props) {
@@ -10,8 +14,27 @@ export default class Product extends Component {
 
     render() {
         return (
-            <Container fluid>
-                <div>Product</div>
+            <Container fluid id="bg">
+                <Row id="Product">
+                </Row>
+                <Row id="Rowgroup-product">
+                    <Col xs={14} md={20} xl={20}>
+                        ใบตัดโลหะ
+                        </Col>
+                    <Col id="List-all" xs={10} md={4} xl={4}>
+                        ดูทั้งหมด
+                        </Col>
+                </Row>
+                <ProductSlide />
+                <Row id="Rowgroup-product">
+                    <Col xs={14} md={20} xl={20}>
+                        ใบตัดโลหะ
+                        </Col>
+                    <Col id="List-all" xs={10} md={4} xl={4}>
+                        ดูทั้งหมด
+                        </Col>
+                </Row>
+                <ProductSlide />
             </Container>
         )
 
