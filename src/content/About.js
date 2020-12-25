32607ec0370b4002874c07dd7783edbe91367abc
Nattarika-Cha <s5902041620113@email.kmutnb.ac.th>
2020-12-25 23:23:13 +0700

    About

--- src/content/About.js
+++ src/content/About.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
-import { Container } from 'react-bootstrap';
-
+import { Container, Image } from 'react-bootstrap';
+import { Row, Col, Divider } from 'antd';
+import '../css/About.css';
+import logo from '../img/hits_logo2.png'
 export default class Abount extends Component {
     constructor(props) {
         super(props);
@@ -11,7 +13,24 @@ export default class Abount extends Component {
     render() {
         return (
             <Container fluid>
-                <div>Abount</div>
+                 <Row id="About">
+                     <Col xs={24} md={24} xl={24} id="col-imglogo">
+                        <Image
+                            src={logo}
+                            width="20%"
+                            height="auto"
+                            className="d-inline-block align-top"
+                            alt="React Bootstrap logo"
+                            style={{ paddingBottom: "0.3%", paddingTop: "0.3%" }}
+                        />
+                     </Col>
+                     <Col id="col-about">
+                        <Divider orientation="left">บริษัท ฮิตส์ จำกัด</Divider>
+                            <p>
+                            hits ผู้ผลิตและผู้จัดจำหน่ายสินค้านำเข้าจากต่างประเทศที่ได้คุณภาพ มาตรฐานระดับสากล ด้วยประสบการณ์ที่มากกว่า 30 ปี ในวงการวัสดุอุปกรณ์ เครื่องมือช่างตกแต่งบ้าน และเฟอร์นิเจอร์ เราคัดสรรสินค้าที่มีคุณภาพเป็นมาตรฐานออกสู่ตลาด รับประกันคุณภาพและบริการ ได้ผลตอบรับที่ดีและความไว้วางใจจากช่างมืออาชีพ วิศวกร สถาปนิก โครงการชั้นนำ โรงงาน และร้านค้าตัวแทนจำหน่าย จากนี้และในอนาคตเรามุ่งมั่นพัฒนาสินค้าและบริการที่ดีสู่คุณ
+                            </p>
+                     </Col>
+                 </Row>
             </Container>
         )
 
