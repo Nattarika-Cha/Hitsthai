31332155b160f5555eda1ef352312886b4efd8fc
Nattarika-Cha <s5902041620113@email.kmutnb.ac.th>
2021-01-20 23:48:25 +0700

    UX/UI

--- src/template/LiveChat.js
+++ src/template/LiveChat.js
@@ -32,38 +32,38 @@ export default class Footer extends Component {
                 style={{ position: 'fixed', bottom: '20px', right: '3%', zIndex: '100000' }}
             >
                 <MainButton
-                    iconResting={<Tooltip placement="left" title="ติดต่อเพิ่มเติม"><MdForum style={{ fontSize: 50, color: "aliceblue", padding: "20%" }} /></Tooltip>}
-                    iconActive={<Tooltip placement="left" title="ติดต่อเพิ่มเติม"><FaTimes style={{ fontSize: 50, color: "aliceblue", padding: "20%" }} /></Tooltip>}
+                    iconResting={<Tooltip placement="left" title="ติดต่อเพิ่มเติม"><MdForum style={{ fontSize: 40, color: "aliceblue", padding: "20%" }} /></Tooltip>}
+                    iconActive={<Tooltip placement="left" title="ติดต่อเพิ่มเติม"><FaTimes style={{ fontSize: 40, color: "aliceblue", padding: "20%" }} /></Tooltip>}
                     style={{ backgroundColor: '#FF9C00' }}
-                    size={60}
+                    size={50}
                     onClick={() => this.setState({ isOpen: !this.state.isOpen })}
                 />
 
                 <ChildButton
-                    icon={<Tooltip placement="left" title="ติดต่อเรา"><NavLink to="/Contact"><FaMapMarkerAlt name="comment" style={{ fontSize: 50, color: "aliceblue", padding: "20%" }} /></NavLink></Tooltip>}
+                    icon={<Tooltip placement="left" title="ติดต่อเรา"><NavLink to="/Contact"><FaMapMarkerAlt name="comment" style={{ fontSize: 40, color: "aliceblue", padding: "20%" }} /></NavLink></Tooltip>}
                     background='darkred'
-                    size={50}
+                    size={45}
                     //onClick={() => { window.location = "/Request"; }}
                 />
 
                 <ChildButton
-                    icon={<Tooltip placement="left" title="โทรศัพท์"><FaPhoneAlt style={{ fontSize: 50 , color: "aliceblue", padding: "20%" }} /></Tooltip>}
+                    icon={<Tooltip placement="left" title="โทรศัพท์"><FaPhoneAlt style={{ fontSize: 40 , color: "aliceblue", padding: "20%" }} /></Tooltip>}
                     background='teal'
-                    size={50}
+                    size={43}
                     onClick={() => { window.open("tel:+66905436000"); }}
                 />
 
 
                 <ChildButton
-                    icon={<Tooltip placement="left" title="Line"><FaLine style={{ fontSize: 60, color: "aliceblue", padding: "20%" }} /></Tooltip>}
+                    icon={<Tooltip placement="left" title="Line"><FaLine style={{ fontSize: 50, color: "aliceblue", padding: "20%" }} /></Tooltip>}
                     background='#00C300'
-                    size={50}
+                    size={43}
                     onClick={() => { window.open("http://line.me/ti/p/~hitsthai", "_blank"); }}
                 />
                 <ChildButton
                     icon={<Tooltip placement="left" title="Facebook"><FaFacebookMessenger style={{ fontSize: 50, color: "aliceblue", padding: "20%" }} /></Tooltip>}
                     background='blue'
-                    size={50}
+                    size={40}
                     onClick={() => { window.open("https://m.me/hits.thai.9", "_blank"); }}
                 />
                 {/* <ChildButton
